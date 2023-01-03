52efcd6b232c8d2a1c86bc4a91cd8c2f0b58ad79
Harshit77Nav <harshit77navneet@gmail.com>
2023-01-03 20:30:45 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 var jwt = require('jsonwebtoken')
+const cors = require('cors')
 
+app.use(cors())
 
 // Import routes
 const orderRoute = require("./routes/order_R");
